73bc235cd8b32294bfe7523a3a6669c3aa2e095a
Aditya Mishra <56506651+Ciriously@users.noreply.github.com>
2025-12-08 23:04:52 +0530

    chore: update manifest with VAPID sender id

--- public/clevertap_sw.js
+++ public/clevertap_sw.js
@@ -3,11 +3,12 @@ importScripts("https://d2r1yp2w7bby2u.cloudfront.net/js/clevertap_sw.js");
 self.addEventListener("notificationclick", function (event) {
   var notification = event.notification;
   var action = event.action;
-  var link = notification.data.deepLink; // Handles deep links from dashboard
+  var link = notification.data.deepLink;
 
   if (action === "close") {
     notification.close();
   } else {
+    // Open the link (or Home if none provided)
     clients.openWindow(link || "/");
     notification.close();
   }
